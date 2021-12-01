890190972ac3486f81c89a7132ff59d65ad87305
am22ar <amarpatil12222@gmail.com>
2021-12-01 15:13:11 +0530

    Added: Area of Circle.

---
+++ areaofcirc/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace areaofcirc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r;
+            Console.Write("enter radius: ");
+            r = Convert.ToDouble(Console.ReadLine());
+            double a = Math.PI * r * r;
+            Console.Write("area of circle: " + a.ToString("#.00"));
+                Console.Read();
+        }
+    }
+}
